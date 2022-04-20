98bb23fe92343ee22072a8cc752147ecb85e24a0
Your Name <luktvpl@wp.pl>
2022-04-20 17:11:19 +0200

    można zainstalować jako pwa

--- main.js
+++ main.js
@@ -1199,3 +1199,10 @@ fetch(
   }
 );
 }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })}
